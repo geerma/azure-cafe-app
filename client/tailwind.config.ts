b868d5d8799b1dc900c6655184b49c7f2707de6c
geerma <geerm1997@gmail.com>
2024-04-01 00:24:00 -0600

    Add to catalog menu and start order page

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -23,6 +23,14 @@ const config: Config = {
       gray: "#42413b",
       yellow: "#FFC226",
     },
+    screens: {
+      xs: "450px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
