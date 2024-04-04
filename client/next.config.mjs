915f755f9ca95d6a9545ab69314bb1b9cf466eb6
geerma <geerm1997@gmail.com>
2024-04-03 22:10:24 -0600

    Set up Azure Blob Storage and Azure CDN

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["azure-cafe-app-cdn-bze0fcdmcme5cgea.z01.azurefd.net"],
+  },
+};
 
 export default nextConfig;
